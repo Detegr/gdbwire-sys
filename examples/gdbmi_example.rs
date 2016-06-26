4c0425075f620709f952459d92d793a3e4d9531f
Antti Keränen <detegr@gmail.com>
2016-06-26 18:00:20 +0300

    Add gdbmi_example.rs

---
+++ examples/gdbmi_example.rs
@@ -0,0 +1,48 @@
+/* The same example that gdbwire has, with Rust FFI bindings */
+
+extern crate gdbwire_sys;
+
+use std::ffi::{CStr, CString};
+use std::os::raw;
+use std::ptr;
+
+fn main() {
+    let callbacks = gdbwire_sys::gdbmi_parser_callbacks {
+        context: ptr::null_mut(),
+        gdbmi_output_callback: Some(parser_callback),
+    };
+    unsafe {
+        let parser = gdbwire_sys::gdbmi_parser_create(callbacks);
+        assert!(!parser.is_null());
+        main_loop(parser);
+        gdbwire_sys::gdbmi_parser_destroy(parser);
+    }
+}
+
+unsafe fn main_loop(parser: *mut gdbwire_sys::gdbmi_parser) {
+    let mut line = String::new();
+    loop {
+        if let Ok(size) = std::io::stdin().read_line(&mut line) {
+            let cline = CString::new(line.clone()).unwrap();
+            let result = gdbwire_sys::gdbmi_parser_push_data(parser, cline.as_ptr(), size);
+            assert!(result == gdbwire_sys::gdbwire_result::GDBWIRE_OK);
+        } else {
+            break;
+        }
+        line.clear();
+    }
+}
+
+unsafe extern "C" fn parser_callback(context: *mut raw::c_void, output: *mut gdbwire_sys::gdbmi_output) {
+    assert!(context.is_null());
+    assert!(!output.is_null());
+
+    if (*output).kind == gdbwire_sys::gdbmi_output_kind::GDBMI_OUTPUT_PARSE_ERROR {
+        let line = CStr::from_ptr((*output).line);
+        println!("\n  Parse Error: {}", line.to_string_lossy());
+    }
+
+    assert!((*output).kind != gdbwire_sys::gdbmi_output_kind::GDBMI_OUTPUT_PARSE_ERROR);
+    assert!((*output).next.is_null());
+    gdbwire_sys::gdbmi_output_free(output);
+}
